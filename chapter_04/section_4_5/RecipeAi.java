c5aa7d8395175b11918414cd5eed233093b6992a
LAPTOP-5HKGILB5\academia <202607_aw_paku@x-ide.sakura.ne.jp>
2026-09-24 15:09:19 +0900

    20260924チャプター４終了

---
+++ chapter_04/section_4_5/RecipeAi.java
@@ -0,0 +1,14 @@
+package chapter_04.section_4_5;
+
+/*
+ * カレーを作る手順を、順番に画面へ表示するプログラムです。
+ */
+public class RecipeAi {
+    public static void main(String[] args) {
+        // カレーを作る手順を表示します。
+        System.out.println("野菜を切る");
+        System.out.println("肉と野菜を炒める");
+        System.out.println("水を入れて煮込む"); // 煮込んだあとにルウを入れます。
+        System.out.println("ルウを溶かす");
+    }
+}
