af8b54b390592aa60c8d0d62ed62833e790481ea
LAPTOP-5HKGILB5\academia <202607_aw_paku@x-ide.sakura.ne.jp>
2026-09-25 10:14:14 +0900

    BookLog コミット

---
+++ chapter_05/section_5_2/BookLogAi.java
@@ -0,0 +1,17 @@
+package chapter_05.section_5_2;
+
+public class BookLogAi {
+    public static void main(String[] args) {
+        String title = "はじめてのJava";
+        int pages = 240;
+        double price = 2860.0;
+        boolean finished = false;
+
+        title = "Javaの絵本";
+
+        System.out.println("書名: " + title);
+        System.out.println("ページ数: " + pages);
+        System.out.println("価格: " + price);
+        System.out.println("読み終わったか: " + finished);
+    }
+}
